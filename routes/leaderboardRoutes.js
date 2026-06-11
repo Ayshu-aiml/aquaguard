570318484001c4504c8c0dc84fe2a2b1d15ba8b7
Ayshu-aiml <konchadaharshitha690@gmail.com>
2026-06-11 15:03:02 +0530

    Fix leaderboard router

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -1,29 +1,25 @@
-
 import express from "express";
 import Volunteer from "../models/Volunteer.js";
 
+const router = express.Router();
+
+// GET Leaderboard
 router.get("/", async (req, res) => {
+  try {
+    const volunteers = await Volunteer.find()
+      .sort({ points: -1 });
 
-    try {
+    res.json({
+      success: true,
+      volunteers
+    });
 
-        const volunteers =
-        await Volunteer.find()
-        .sort({ points: -1 });
-
-        res.json({
-            success: true,
-            volunteers
-        });
-
-    } catch(error) {
-
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-
-    }
-
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
 });
 
 export default router;
